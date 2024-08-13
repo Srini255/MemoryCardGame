307f23ecdfd08e490a08267e46a3d0cf8e7a00ed
srini255 <srinitamil17@gmail.com>
2024-08-13 19:21:15 +0530

    changed cards and added transformation partial of project done

--- js/script.js
+++ js/script.js
@@ -1,16 +1,25 @@
 let a=["img1.png","img2.jpg","img3.jpg","img4.png","img1.png","img2.jpg","img3.jpg","img4.png"]
-
+let matches=[];
+let check=[];
 document.addEventListener('DOMContentLoaded',()=>
 {
+    startGame();
+});
+
+
+function startGame(){
     shuffle();
-    const cards=document.querySelectorAll('.img');
+    const cards=document.querySelectorAll('#img');
     let i=0;
     cards.forEach(card=>{
         card.id=`${a[i]}`;
-        card.style.content=`url("../img/${a[i]}")`;
+        card.className='a-img';
+        card.src=`img/${a[i]}`;
+        // card.style.width="200px";
+        // card.style.height="200px";
         i++;
     });
-});
+}
 
 function shuffle(){
     for(let i=a.length-1;i>0;i--)
@@ -28,13 +37,6 @@ document.querySelectorAll('.back').forEach(image=>{
     })
 })
 
-setInterval(()=>{
-    document.querySelectorAll('.img').forEach(image=>{
-            if(image.style.display==="block"){
-            image.style.display="none";
-            }
-        });
-},"10000")
 
 
 document.getElementById('start').addEventListener('click',()=>{
@@ -53,3 +55,52 @@ document.getElementById('start').addEventListener('click',()=>{
         Count.textContent=Count.value;
     },"1000")
 })
+
+
+document.querySelectorAll('.flip-card').forEach(card=>{
+    card.addEventListener('click',()=>{
+        const flip=card.querySelector('.flipcard-inner');   
+        if(check.length===2){
+            compare();
+        }
+        flip.classList.contains("inner")?flip.classList.remove("inner"):flip.classList.add("inner");
+        check.push(flip);
+        if(matches.length==6&&check.length===2){
+            compare();}
+    })
+    
+})
+
+function compare(){
+    const first=check[0].children[1].children[0].id;
+    const second=check[1].children[1].children[0].id;
+    if(first===second){
+        check.forEach(ele=>{
+            matches.push(ele)});
+        check=[];
+        if(matches.length===a.length)
+        {
+            alert("Congratulations!you won the game");
+        }
+    }else{ 
+        check.forEach(ele=>{
+            ele.classList.remove("inner");
+        })
+        check=[];
+    }
+}
+
+function reset(){
+    matches=[];
+    document.querySelectorAll('.inner').forEach(ele=>{
+        ele.classList.remove("inner");
+    });
+    const cards=document.querySelectorAll('.a-img');
+    let i=0;
+    cards.forEach(card=>{
+    card.id="img";
+    card.classList.remove('a-img');
+    card.removeAttribute('src');
+    });
+    startGame();
+}
